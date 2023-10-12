acb0ef944e18a43a6a2bf3446b08fea9aee9746b
Lucky <luckykashyap3430@gmail.com>
2023-10-12 14:26:07 +0530

    basic terminology

--- front end/Javascript/js/script.js
+++ front end/Javascript/js/script.js
@@ -31,14 +31,83 @@
 // reference mein copy reference or memory address
 // [] {} ()
 
-var a = [1,2,3];
-
+// var a = [1, 2, 3];
 
 // var b = a;
 
+// var b = [...a]; // real copy data
 
-var b = [...a];  // real copy data
+// b.push(4, 5, 6);
 
-b.push(4,5,6);
+// console.log(a, b);
 
-console.log(a,b);
+// day 45
+
+// 1. Primitve -
+//- Number (343,445,3,53)
+//- float (3.43,44,5,3.1,5,3)
+//- double (333333333333345543.434455)
+//- Null
+//- undefined
+// - Boolean
+//- character
+//- String
+
+// 2. Reference-
+//- Array
+//- Functions
+//- Objects
+
+// Game of Brackets
+
+// () - parenthesis
+// {} - curly braces
+// [] - square bracket
+
+// () - function create or call or method  ....
+
+// apple()   - it is a function
+// chacha()  - it is also a function
+
+// {} -  scope, object .....
+// {
+// scope   particluar region where they exist or can be recognized
+// }
+
+// India - Lucky
+
+// Russia - Daniel
+
+// [] - array
+
+// multiple values we can store
+
+// conditianals
+
+// true/false yes/no
+
+// - if it is a football then kick it - conditional
+// - Kutte bhok rhe ha - statement
+// - kya apne pichla lecture dekha  - conditional
+
+// example
+
+// graphcal()- fn/m
+// mahatma gandhi() fn/m
+// lost() - fn/m
+// {
+//     var a  = 1; scope/blank object
+// }
+
+// avbg = [1,2,3,4] - array
+
+// uses of function & why they are....
+// functiona is just a way to name some amount of code so that we can recall/resue those lines of code
+// again and again without writing t many times
+
+// resubale
+// functionality
+// separation of concern
+// for separate functionality in code
+// particular work - particular function
+// block of code
