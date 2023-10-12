855c1df2367b0ddd52591f4c1cad444c2f91893d
Lucky <luckykashyap3430@gmail.com>
2023-10-12 17:42:21 +0530

    conditional statement & basics of array

--- front end/Javascript/JS2/js/script.js
+++ front end/Javascript/JS2/js/script.js
@@ -115,3 +115,116 @@
 // else
 // if | else if | else
 // ternary operator
+// array
+
+// day 48 JS 5
+
+// conditionals
+
+// if else else if
+
+// jab situation kuch aisi ho, "AGAR" aisa ho to aisa kro "nhi" to esa kro
+
+// if(true)
+
+// any value which is true we can pass inside if()
+
+// var someNumber = 20;
+
+// if (5 > 4) {
+//   console.log("greater");
+// } else {
+//   console.log("Less value");
+// }
+
+// ask user a name if its 'lucky' say congrats
+
+// var user_name = prompt('User name');
+
+// if(user_name === 'Lucky'){
+//     console.log('Congrats');
+// }
+// else{
+//     console.log('wrong value enter');
+// }
+
+// ask user a name if its 'lucky' say congrats , if its "Sumit" say hi, if smething
+// else else say try again
+
+// var user_name = prompt("User name");
+
+// if (user_name === "Lucky") {
+//   console.log("Congrats");
+// }
+// if (user_name === "Lucky") {
+//   console.log("Hi");
+// }
+// if(user_name==='Sumit'){
+//     console.log('Hi');
+// }
+// else {
+//   console.log("wrong value enter");
+// }
+
+// if(user_name === 'Lucky'){
+//     console.log('Congrats');
+// }
+// else if(user_name==='Sumit'){
+//     console.log('Hi');
+// }
+// else{
+//     console.log('wrong value enter');
+// }
+
+// ternary
+
+// single line
+// condition ? do something f condition went true: do something  if false
+
+// var ans = 5 > 4 ? console.log("greater") : console.log("Less value");
+
+// console.log(ans);
+
+// app login email => abcd@gmail.com pass=> 123456
+
+// if(email == database.email){
+//     if(pass == database.password){
+//         // perform login
+//     }else{
+//         console.log('wrong password');
+//     }
+// }
+// else{
+// console.log('invalid email');
+// }
+
+// array
+// when you have multiple values to store
+
+// when you have more than one details to store, and both of those
+// details are related to each other, at this point of time you can store
+// in something called Array
+
+// a data storage whch can store one or more than one data together
+
+// take 5 username
+
+// var arr = [1, true, undefined, null, "string"];
+
+// console.log(arr);
+
+// var u1 = 'Lucky';
+// var u2 = 'Sumit';
+// var u3 = 'Amar';
+// var u4 = 'Trijal'
+// var u5 = 'Ajay';
+
+// var user_name = [u1,u2,u3,u4,u5];
+
+// console.log(user_name);
+
+// access using array index
+
+// console.log(user_name[3]);
+
+// console.log(user_name[-1]); // undefined
