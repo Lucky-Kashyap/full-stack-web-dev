08cbe45cebf4f1b100011438bd36ddb15e166514
Lucky <luckykashyap3430@gmail.com>
2023-10-12 12:18:13 +0530

    understanding variable & datatypes

--- front end/Javascript/js/script.js
+++ front end/Javascript/js/script.js
@@ -1,3 +1,44 @@
 //JS
 
-console.log('JS');
+// console.log('JS');
+
+// alert('Hello');
+
+// rule set   (game)
+// score
+
+//  Raja Rani + 1
+
+// end game above 95
+
+// varibale : space to store some value
+
+// types  -
+// two types :
+// primitive & reference
+
+// string ,number,boolean,undefined,null,symbol,object
+// bigint
+
+// var a = 20;
+
+// var b = a;
+
+// b=b+2;
+
+// console.log(a,b);
+
+// reference mein copy reference or memory address
+// [] {} ()
+
+var a = [1,2,3];
+
+
+// var b = a;
+
+
+var b = [...a];  // real copy data
+
+b.push(4,5,6);
+
+console.log(a,b);
