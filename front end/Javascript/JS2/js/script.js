860c88f394efbc78d8f0e59bab6f847fb062651c
Lucky <luckykashyap3430@gmail.com>
2023-10-12 15:30:41 +0530

    variables,constants,alert-box & understand boiler plate code of css

---
+++ front end/Javascript/JS2/js/script.js
@@ -0,0 +1,60 @@
+// variables
+// it is a space where you can put some value for the program lifecycle
+// dynamic language
+// datatype decide on runtime
+
+// var  keyword to declare variable
+
+// var a = 57;
+
+// var dulha = "chimku";
+// var dulhan = "chimki";
+
+// // var ex = "Badal";
+
+// // chimku weds chimki
+
+// console.log(dulha + " weds " + dulhan);
+
+// dulha = "Badal";
+
+// console.log(dulha + " weds " + dulhan);
+
+// const a = 57;
+
+// const dulha = "chimku";
+// const dulhan = "chimki";
+
+// // const ex = "Badal";
+
+// // chimku weds chimki
+// // const dulha = "chimku";
+// console.log(dulha + " weds " + dulhan);
+
+// dulha = "Badal";
+
+// console.log(dulha + " weds " + dulhan);
+
+// How complier work
+
+// var a = 21;
+// var b;
+
+// console.log(a,b);
+
+// Js work
+
+// alert
+
+// pop up
+
+// alert('Pop Up)   // - method
+
+// var a = "Hacked";
+
+// alert(a);
+
+// prompt
+//console.log()
+// console.error()
+// console.warn()
