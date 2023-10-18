edf0506c2ade1ed6da2a4e6e0e189e6655cdc382
Lucky <luckykashyap3430@gmail.com>
2023-10-18 14:16:31 +0530

    add logic for show or hide card

--- front end/Javascript/Modal/js/script.js
+++ front end/Javascript/Modal/js/script.js
@@ -11,3 +11,17 @@
 
 // heading.style.backgroundColor='orange';
 // heading.style.color='red';
+
+var cross = document.querySelector("span");
+var card = document.querySelector(".card");
+var button =document.querySelector('button');
+
+cross.addEventListener("click", () => {
+  card.classList.remove("show");
+});
+
+
+button.addEventListener('click',()=>{
+    // card.style.display='block';
+    card.classList.add('show');
+})
