20c95bda42f191674e036b04256c80f7c2bbeb6b
Lucky <luckykashyap3430@gmail.com>
2023-10-18 22:01:19 +0530

    add toggle functionality

--- front end/Javascript/Bulb on OFF/js/script.js
+++ front end/Javascript/Bulb on OFF/js/script.js
@@ -1,13 +1,18 @@
 console.log("Bulb ON OFF");
 
-let on = document.querySelector(".on");
-let off = document.querySelector(".off");
+// let on = document.querySelector(".on");
+// let off = document.querySelector(".off");
 let bulb = document.querySelector(".bulb");
+let toggleBtn = document.querySelector('.toggle');
 
-on.addEventListener("click", () => {
-  bulb.classList.add("active");
-});
+// on.addEventListener("click", () => {
+//   bulb.classList.add("active");
+// });
 
-off.addEventListener("click", () => {
-  bulb.classList.remove("active");
-});
+// off.addEventListener("click", () => {
+//   bulb.classList.remove("active");
+// });
+
+toggleBtn.addEventListener('click',()=>{
+  bulb.classList.toggle('active');
+})
