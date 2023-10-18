f41706794dc6c2235fde95de504948efcb7806f6
Lucky <luckykashyap3430@gmail.com>
2023-10-18 13:58:07 +0530

    UI created for modal

---
+++ front end/Javascript/Modal/js/script.js
@@ -0,0 +1,13 @@
+// selection
+// change
+
+// console.log(document.documentElement);
+
+// change heading
+
+// document.querySelector('h1').innerHTML='MODAL PRACTICE';
+
+// let heading = document.querySelector('h1');
+
+// heading.style.backgroundColor='orange';
+// heading.style.color='red';
