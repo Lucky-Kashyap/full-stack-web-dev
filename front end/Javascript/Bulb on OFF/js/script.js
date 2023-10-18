3b4a9064814b6b7050bcd396949e8f539bd391d7
Lucky <luckykashyap3430@gmail.com>
2023-10-18 22:21:32 +0530

    with logic implement toggle functionality

--- front end/Javascript/Bulb on OFF/js/script.js
+++ front end/Javascript/Bulb on OFF/js/script.js
@@ -3,7 +3,7 @@ console.log("Bulb ON OFF");
 // let on = document.querySelector(".on");
 // let off = document.querySelector(".off");
 let bulb = document.querySelector(".bulb");
-let toggleBtn = document.querySelector('.toggle');
+let toggleBtn = document.querySelector(".toggle");
 
 // on.addEventListener("click", () => {
 //   bulb.classList.add("active");
@@ -13,6 +13,27 @@ let toggleBtn = document.querySelector('.toggle');
 //   bulb.classList.remove("active");
 // });
 
-toggleBtn.addEventListener('click',()=>{
-  bulb.classList.toggle('active');
-})
+var lightStatus = false;
+
+toggleBtn.addEventListener("click", () => {
+  // bulb.classList.toggle("active");
+
+  if(lightStatus == false){
+    bulb.style.backgroundColor='orange';
+    lightStatus=true;
+  }
+  else{
+    bulb.style.backgroundColor='#fff';
+    lightStatus=false;
+  }
+
+
+
+  // lightStatus=true;
+
+  // if(lightStatus){
+  //   toggleBtn.innerHTML="OFf Bulb";
+  //   lightStatus=false;
+  // }
+  // toggleBtn.classList.toggle('add');
+});
