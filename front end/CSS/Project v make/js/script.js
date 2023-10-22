c71455d8abb292cb6327fed525d4255642512052
Lucky <luckykashyap3430@gmail.com>
2023-10-22 11:57:35 +0530

    created use case section

---
+++ front end/CSS/Project v make/js/script.js
@@ -0,0 +1,17 @@
+
+
+// tab switch
+
+// let usecase1 = document.querySelector('#use-case-1');
+// let usecase2 = document.querySelector('#use-case-2');
+// let usecase3 = document.querySelector('#use-case-3');
+
+
+
+// let box1 = document.querySelector('.box-1');
+
+
+// box1.addEventListener('click',()=>{
+//     usecase2.classList.add('hide');
+//     usecase3.classList.add('hide');
+// })
