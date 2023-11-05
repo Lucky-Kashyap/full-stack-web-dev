90e536d5a31d75466fcf067fa7d063661acf0807
Lucky <luckykashyap3430@gmail.com>
2023-11-05 16:40:34 +0530

    todo UI created

--- front end/React JS/projects/todo-app-v1/src/App.jsx
+++ front end/React JS/projects/todo-app-v1/src/App.jsx
@@ -2,9 +2,36 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-center">Todo App</h1>
-    </>
+    <div className="container text-center">
+      <h1>Todo App</h1>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">
+            <input type="text" placeholder="Enter data" />
+          </div>
+          <div className="col-4">
+            <input type="date" />
+          </div>
+          <div className="col-2">
+            <button className="btn btn-success">Add</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">Buy Milk</div>
+          <div className="col-4">05/11/2023</div>
+          <div className="col-2">
+            <button className="btn btn-danger">Delete</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">Go to College</div>
+          <div className="col-4">05/11/2023</div>
+          <div className="col-2">
+            <button className="btn btn-danger">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
