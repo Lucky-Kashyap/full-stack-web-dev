7859de19c18907160aed052103e225cfd31fa7d5
Lucky <luckykashyap3430@gmail.com>
2023-11-05 15:04:07 +0530

    create footer component

--- front end/React JS/create-react-comp/src/App.jsx
+++ front end/React JS/create-react-comp/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <p>creating component</p>
       <Header />
+      <Footer />
     </>
   );
 }
