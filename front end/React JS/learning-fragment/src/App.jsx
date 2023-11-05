50e1b8ef27ec9645655959ff663fa4aef3d55d5f
Lucky <luckykashyap3430@gmail.com>
2023-11-05 17:55:16 +0530

    learning fragment

---
+++ front end/React JS/learning-fragment/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>React Fragments</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        natus rerum aliquid recusandae odit, magni nemo, corporis cumque ipsa
+        alias neque assumenda nobis, minus impedit! Odit quia necessitatibus
+        dolorem quis?
+      </p>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+        <li>Testimonials</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
