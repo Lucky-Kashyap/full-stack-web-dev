a7bd69c2bf70de1cad0114164a1b76e73a2974d0
Lucky <luckykashyap3430@gmail.com>
2023-11-05 14:42:08 +0530

    creating component

---
+++ front end/React JS/create-react-comp/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <p>creating component</p>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ front end/React JS/create-react-comp/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <>
+      <h1>Header Component</h1>
+    </>
+  );
+}
+
+export default Header;
