1fba20c61a750438c9f5f02d0c321e3efb1aa759
Lucky <luckykashyap3430@gmail.com>
2023-11-06 14:30:53 +0530

    use logical operator for rendering

--- front end/React JS/learning-fragment/src/components/MapList.jsx
+++ front end/React JS/learning-fragment/src/components/MapList.jsx
@@ -1,16 +1,24 @@
 const MapList = () => {
-  // const foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+  const foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
 
-  const foodItems = [];
+  // const foodItems = [];
 
-  if (foodItems.length === 0) {
-    return <h3>I am still hungry</h3>;
-  }
+  // if (foodItems.length === 0) {
+  //   return <h3>I am still hungry</h3>;
+  // }
+
+  // const emptyMessage =
+  // foodItems.length === 0 ? <h3>I am Still Hungry.</h3> : null;
 
   return (
     <>
       <h3>Food</h3>
 
+      {/* {foodItems.length === 0 ? <h3>I am Still Hungry.</h3> : null} */}
+
+      {/* {emptyMessage} */}
+
+      {foodItems.length === 0 && <h3>I am Still Hungry</h3>}
       <ul className="list-group">
         {foodItems.map((item, i) => (
           <li className="list-group-item" key={i}>
