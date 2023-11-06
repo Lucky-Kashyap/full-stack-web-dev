ad60cbb1cbc507c97ade604403df7d409867ec70
Lucky <luckykashyap3430@gmail.com>
2023-11-06 18:31:26 +0530

    add todo item to list on your own

--- front end/React JS/projects/todo-app-v2/src/App.jsx
+++ front end/React JS/projects/todo-app-v2/src/App.jsx
@@ -1,31 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import AddTodo from "./components/AddTodo";
-import AddTodoItem1 from "./components/AddTodoItem1";
-import AddTodoItem2 from "./components/AddTodoItem2";
 import Heading from "./components/Heading";
 import TodoItems from "./components/TodoItems";
 
 function App() {
-  const todoItems = [
+  const [todoItems, setTodoItems] = useState([
     {
       name: "Buy Milk",
       dueDate: "4/10/2023",
     },
-    {
-      name: "Go to College",
-      dueDate: "4/10/2023",
-    },
-    {
-      name: "Like this video",
-      dueDate: "right now",
-    },
-  ];
+  ]);
 
   return (
     <div className="container text-center">
       <Heading />
       <div className="container text-center">
-        <AddTodo />
+        <AddTodo todoItems={todoItems} setTodoItems={setTodoItems} />
 
         <TodoItems todoItems={todoItems} />
 
--- front end/React JS/projects/todo-app-v2/src/components/AddTodo.jsx
+++ front end/React JS/projects/todo-app-v2/src/components/AddTodo.jsx
@@ -1,15 +1,39 @@
-const AddTodo = () => {
+import { useRef } from "react";
+
+const AddTodo = ({ todoItems, setTodoItems }) => {
+  const item = useRef(null);
+  const date = useRef(null);
+
+  const addTodo = () => {
+    // setTodoItems({
+    //   name: item.current.value,
+    //   date: date.current.value,
+    //   ...todoItems,
+    // });
+
+    const todo = {
+      name: item.current.value,
+      dueDate: date.current.value,
+    };
+
+    console.log(todo);
+
+    setTodoItems([...todoItems, todo]);
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-6">
-          <input type="text" placeholder="Enter data" />
+          <input type="text" ref={item} placeholder="Enter data" />
         </div>
         <div className="col-4">
-          <input type="date" />
+          <input type="date" ref={date} />
         </div>
         <div className="col-2">
-          <button className="btn btn-success add">Add</button>
+          <button className="btn btn-success add" onClick={addTodo}>
+            Add
+          </button>
         </div>
       </div>
     </div>
