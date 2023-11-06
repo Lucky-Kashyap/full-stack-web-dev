b2f3af8b396783da1bd4f31f4d414a72842b5998
Lucky <luckykashyap3430@gmail.com>
2023-11-06 21:45:59 +0530

    basic calculator UI created

--- front end/React JS/projects/calculator-app/src/App.jsx
+++ front end/React JS/projects/calculator-app/src/App.jsx
@@ -1,10 +1,29 @@
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-center fw-bolder">Calculator</h1>
-    </>
+    <div className={styles.calculator}>
+      <input type="text" placeholder="number" className={styles.display} />
+      <div className={styles.buttonsContainer}>
+        <button className={styles.button}>C</button>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>+</button>
+        <button className={styles.button}>3</button>
+        <button className={styles.button}>4</button>
+        <button className={styles.button}>-</button>
+        <button className={styles.button}>5</button>
+        <button className={styles.button}>6</button>
+        <button className={styles.button}>*</button>
+        <button className={styles.button}>7</button>
+        <button className={styles.button}>8</button>
+        <button className={styles.button}>/</button>
+        <button className={styles.button}>=</button>
+        <button className={styles.button}>9</button>
+        <button className={styles.button}>0</button>
+        <button className={styles.button}>.</button>
+      </div>
+    </div>
   );
 }
 
