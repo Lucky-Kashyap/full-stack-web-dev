3adbc3ff862f04ad977ecbb980a70c8bc2994d70
Lucky <luckykashyap3430@gmail.com>
2023-11-07 14:57:53 +0530

    passing children

--- front end/React JS/learning-fragment/src/App.jsx
+++ front end/React JS/learning-fragment/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Container from "./components/Container";
 import DataProp from "./components/DataProp";
 import List from "./components/List";
 import MapList from "./components/MapList";
@@ -6,27 +7,36 @@ import MapList from "./components/MapList";
 function App() {
   return (
     <div>
-      <h1>React Fragments</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-        natus rerum aliquid recusandae odit, magni nemo, corporis cumque ipsa
-        alias neque assumenda nobis, minus impedit! Odit quia necessitatibus
-        dolorem quis?
-      </p>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Contact</li>
-        <li>Testimonials</li>
-      </ul>
+      <Container>
+        <h1>React Fragments</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          natus rerum aliquid recusandae odit, magni nemo, corporis cumque ipsa
+          alias neque assumenda nobis, minus impedit! Odit quia necessitatibus
+          dolorem quis?
+        </p>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+          <li>Testimonials</li>
+        </ul>
 
-      {/* <List /> */}
-      <MapList
-        foodItems={["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"]}
-      />
+        {/* <List /> */}
+        <MapList
+          foodItems={[
+            "Dal",
+            "Green Vegetable",
+            "Roti",
+            "Salad",
+            "Milk",
+            "Ghee",
+          ]}
+        />
 
-      <DataProp name="pass data as a props" description="Learning react js" />
+        <DataProp name="pass data as a props" description="Learning react js" />
+      </Container>
     </div>
   );
 }
