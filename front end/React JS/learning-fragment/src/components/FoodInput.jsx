38bc2ef53c398ae0039f6c2bbe6d39b4649d0fc7
Lucky <luckykashyap3430@gmail.com>
2023-11-07 16:09:02 +0530

    onChange event

--- front end/React JS/learning-fragment/src/components/FoodInput.jsx
+++ front end/React JS/learning-fragment/src/components/FoodInput.jsx
@@ -1,11 +1,16 @@
 import styles from "./FoodInput.module.css";
 
 const FoodInput = () => {
+  const handleOnChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <input
       type="text"
       placeholder="Enter Food Item here"
       className={styles.foodInput}
+      // onChange={(e) => console.log(e.target.value)}
+      onChange={handleOnChange}
     />
   );
 };
