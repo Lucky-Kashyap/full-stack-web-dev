cda4c0c3fa673fbaa0e172c3fb597f02bc65039e
Lucky <luckykashyap3430@gmail.com>
2023-11-07 14:40:43 +0530

    convert button elements in list

--- front end/React JS/projects/calculator-app/src/components/ButtonsContainer.jsx
+++ front end/React JS/projects/calculator-app/src/components/ButtonsContainer.jsx
@@ -1,25 +1,31 @@
 import styles from "./ButtonsContainer.module.css";
 
 const ButtonsContainer = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+
   return (
     <div className={styles.buttonsContainer}>
-      <button className={styles.button}>C</button>
-      <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>+</button>
-      <button className={styles.button}>3</button>
-      <button className={styles.button}>4</button>
-      <button className={styles.button}>-</button>
-      <button className={styles.button}>5</button>
-      <button className={styles.button}>6</button>
-      <button className={styles.button}>*</button>
-      <button className={styles.button}>7</button>
-      <button className={styles.button}>8</button>
-      <button className={styles.button}>/</button>
-      <button className={styles.button}>=</button>
-      <button className={styles.button}>9</button>
-      <button className={styles.button}>0</button>
-      <button className={styles.button}>.</button>
+      {buttonNames.map((buttonName) => (
+        <button className={styles.button}>{buttonName}</button>
+      ))}
     </div>
   );
 };
