d128754686f3700a6494cf2d64b9eb4eeb934276
Lucky <luckykashyap3430@gmail.com>
2023-11-11 15:23:16 +0530

    search with query parameters

--- front end/React JS/api handle/backend/index.js
+++ front end/React JS/api handle/backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/api.products", (req, res) => {
+app.get("/api/products", (req, res) => {
   // res.send();
   const products = [
     {
@@ -13,6 +13,19 @@ app.get("/api.products", (req, res) => {
         "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     },
   ];
+
+  if (req.query.search) {
+    const filterProducts = products.filter((product) =>
+      product.name.includes(req.query.search)
+    );
+
+    res.send(filterProducts);
+    return;
+  }
+
+  setTimeout(() => {
+    res.send(products);
+  }, 3000);
 });
 
 const port = process.env.PORT || 8500;
