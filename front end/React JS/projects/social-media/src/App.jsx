5ba2d466a1ce0a6825ccaf8bfbd37d76d6b70d1b
Lucky <luckykashyap3430@gmail.com>
2023-11-24 14:37:52 +0530

    add sidebar component

--- front end/React JS/projects/social-media/src/App.jsx
+++ front end/React JS/projects/social-media/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div>
+      <Sidebar />
       <Header />
+      <Footer />
     </div>
   );
 }
