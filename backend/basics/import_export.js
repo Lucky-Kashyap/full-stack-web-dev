b2f26bd1fc47811f46a583a393d1e7633fda6ca4
Lucky <luckykashyap3430@gmail.com>
2023-11-25 18:44:08 +0530

    learning import export in js

---
+++ backend/basics/import_export.js
@@ -0,0 +1,5 @@
+// import export
+
+var d = 12345;
+
+module.exports = d;
---
+++ backend/basics/script.js
@@ -0,0 +1,11 @@
+var chacha = require("./import_export");
+
+// declare variable
+
+var a = 12;
+
+var b = 22;
+
+var c = 45;
+
+console.log(chacha);
