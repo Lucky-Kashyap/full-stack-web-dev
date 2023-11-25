48a31439fa7304294d6e5f03cf580101191ecbe8
Lucky <luckykashyap3430@gmail.com>
2023-11-25 16:17:45 +0530

    create post component

--- front end/React JS/projects/social-media/src/App.jsx
+++ front end/React JS/projects/social-media/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import CreatePost from "./components/CreatePost";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
@@ -9,6 +10,7 @@ function App() {
       <Sidebar />
       <div className="content">
         <Header />
+        <CreatePost />
         <Footer />
       </div>
     </div>
