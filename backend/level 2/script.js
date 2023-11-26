84825b77ed19a9022cd10744113814edd5dce4ed
Lucky <luckykashyap3430@gmail.com>
2023-11-26 14:59:32 +0530

    create multiple routes

--- backend/level 2/script.js
+++ backend/level 2/script.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Learning Backend wih sheryians");
 });
 
-app.listen(3000);
+app.get("/profile", function (req, res) {
+  res.send("Profile Route");
+});
+
+app.listen(5000);
