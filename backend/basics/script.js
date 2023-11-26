1ecfebbd44383c110db1476b15503d01a04a15ce
Lucky <luckykashyap3430@gmail.com>
2023-11-26 14:12:16 +0530

    install diff package & use it

--- backend/basics/script.js
+++ backend/basics/script.js
@@ -1,11 +1,16 @@
-var chacha = require("./import_export");
+var oneLinerJoke = require("one-liner-joke");
+
+var getRandomJoke = oneLinerJoke.getRandomJoke();
+console.log(getRandomJoke);
+
+// var chacha = require("./import_export");
 
 // declare variable
 
-var a = 12;
+// var a = 12;
 
-var b = 22;
+// var b = 22;
 
-var c = 45;
+// var c = 45;
 
-console.log(chacha);
+// console.log(chacha);
