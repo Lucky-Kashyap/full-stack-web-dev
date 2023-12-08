24e23c07becb2590b70822829a4140d017d1349b
Lucky <luckykashyap3430@gmail.com>
2023-12-08 15:16:40 +0530

    use array of object and map through it

--- front end/React JS/projects/mini docs/src/components/Card.jsx
+++ front end/React JS/projects/mini docs/src/components/Card.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
 
-const Card = () => {
+const Card = ({ data }) => {
   return (
-    <div className="relative w-60 h-72 overflow-hidden rounded-[50px] bg-zinc-900/90 text-white px-8 py-10">
+    <div className="relative flex-shrink-0 w-60 h-72 overflow-hidden rounded-[45px] bg-zinc-900/90 text-white px-8 py-10">
       <FaRegFileAlt />
-      <p className="text-sm leading-tight mt-4 font-semibold">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quos ad
-        aspernatur
-      </p>
-      <div className="footer absolute bottom-0 w-full  py-3 px-8 left-0">
-        <div className="flex items-center justify-between mb-5">
-          <h5>.4mb</h5>
-          <span className="w-5 h-5 bg-sky-200 rounded-full flex items-center justify-center">
-            <LuDownload />
+      <p className="text-sm leading-tight mt-4 font-semibold">{data.desc}</p>
+      <div className="footer absolute bottom-0 w-full left-0">
+        <div className="flex items-center justify-between mb-3 px-8 py-3">
+          <h5>{data.fileSize}</h5>
+          <span className="w-7 h-7 bg-zinc-900 rounded-full flex items-center justify-center">
+            {data.close ? (
+              <IoClose />
+            ) : (
+              <LuDownload size={"3em"} color="#fff" />
+            )}
           </span>
         </div>
+        {data.tag.isOpen && (
+          <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
+            {" "}
+            <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+          </div>
+        )}
       </div>
     </div>
   );
--- front end/React JS/projects/mini docs/src/components/Foreground.jsx
+++ front end/React JS/projects/mini docs/src/components/Foreground.jsx
@@ -2,9 +2,44 @@ import React from "react";
 import Card from "./Card";
 
 const Foreground = () => {
+  // const data = [
+  //   {
+  //     desc,filesize,closeordownload, tagdetails
+  //   }
+  // ]
+
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      fileSize: "9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      fileSize: "43mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      fileSize: "23mb",
+      close: false,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      fileSize: "19mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+  ];
   return (
-    <div className="fixed z-[3] top-0 left-0 w-full h-full">
-      <Card />
+    <div className="fixed z-[3] top-0 left-0 w-full h-full flex gap-5 flex-wrap p-5">
+      {/* <Card /> */}
+      {data.map((item, index) => (
+        <Card data={item} />
+      ))}
     </div>
   );
 };
