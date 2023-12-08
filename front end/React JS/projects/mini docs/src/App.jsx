9cff109e02bcc8de922c6db40cbb7a52863e5eaf
Lucky <luckykashyap3430@gmail.com>
2023-12-08 14:46:51 +0530

    create card component

--- front end/React JS/projects/mini docs/src/App.jsx
+++ front end/React JS/projects/mini docs/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Background from "./components/Background";
+import Foreground from "./components/Foreground";
 
 function App() {
   return (
     <div className="w-full h-screen relative bg-zinc-800">
       <Background />
-      <div className="fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/50"></div>
+      <Foreground />
     </div>
   );
 }
---
+++ front end/React JS/projects/mini docs/src/components/Foreground.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+const Foreground = () => {
+  return (
+    <div className="fixed z-[3] top-0 left-0 w-full h-full">
+      <Card />
+    </div>
+  );
+};
+
+export default Foreground;
