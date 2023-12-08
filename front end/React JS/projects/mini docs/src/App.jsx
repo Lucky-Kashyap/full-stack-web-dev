aebbe166d363ac5c6b76bcf769d75f76e02bad74
Lucky <luckykashyap3430@gmail.com>
2023-12-08 14:23:20 +0530

    create background component

--- front end/React JS/projects/mini docs/src/App.jsx
+++ front end/React JS/projects/mini docs/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Background from "./components/Background";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="w-full h-screen relative bg-zinc-800">
+      <Background />
+      <div className="fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/50"></div>
+    </div>
+  );
 }
 
 export default App;
---
+++ front end/React JS/projects/mini docs/src/components/Background.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div className="fixed z[2] w-full h-screen">
+      <div className="absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold">
+        Documents.
+      </div>
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[12vw] leading-none tracking-tighter font-semibold text-zinc-400">
+        Mini Docs
+      </h1>
+    </div>
+  );
+};
+
+export default Background;
