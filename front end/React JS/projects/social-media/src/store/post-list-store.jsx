457a87ac8e005cfce5fdd85f68d75c1646073634
Lucky <luckykashyap3430@gmail.com>
2023-12-09 18:36:21 +0530

    use useCallback hook

--- front end/React JS/projects/social-media/src/store/post-list-store.jsx
+++ front end/React JS/projects/social-media/src/store/post-list-store.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useCallback, useReducer } from "react";
 
 export const PostList = createContext({
   postList: [],
@@ -51,14 +51,17 @@ const PostListProvider = ({ children }) => {
     });
   };
 
-  const deletePost = (postId) => {
-    dispatchPostList({
-      type: "DELETE_POST",
-      payload: {
-        postId,
-      },
-    });
-  };
+  const deletePost = useCallback(
+    (postId) => {
+      dispatchPostList({
+        type: "DELETE_POST",
+        payload: {
+          postId,
+        },
+      });
+    },
+    [dispatchPostList]
+  );
 
   return (
     <PostList.Provider
