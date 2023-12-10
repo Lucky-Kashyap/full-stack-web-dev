18ece65533e8bcf145c362d1c89b8be7cd4c058d
Lucky <luckykashyap3430@gmail.com>
2023-12-10 15:08:19 +0530

    create userModel

--- backend/level-6/routes/index.js
+++ backend/level-6/routes/index.js
@@ -1,23 +1,25 @@
 var express = require("express");
 var router = express.Router();
 
+const userModel = require("./users");
+
 /* GET home page. */
 router.get("/", function (req, res) {
   res.render("index");
 });
 
 router.get("/failed", function (req, res) {
-  req.flash("age", 23);
-  req.flash("name", "Lucky Kashyap");
+  // req.flash("age", 23);
+  // req.flash("name", "Lucky Kashyap");
   // res.render("index");
 
   res.send("ban gaya data");
 });
 
-router.get("/checkkaro", (req, res) => {
-  console.log(req.flash("age"));
-  console.log(req.flash("name"));
-  res.send("check karlo backend ke terminal par");
-});
+// router.get("/checkkaro", (req, res) => {
+//   console.log(req.flash("age"));
+//   console.log(req.flash("name"));
+//   res.send("check karlo backend ke terminal par");
+// });
 
 module.exports = router;
--- backend/level-6/routes/users.js
+++ backend/level-6/routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:27017/testingendgame2");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  nickname: String,
+  description: String,
+  categories: {
+    type: Array,
+    default: [],
+  },
+  datecreated: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-module.exports = router;
+mongoose.model("user", userSchema);
