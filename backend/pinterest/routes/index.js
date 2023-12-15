9e4a2c71209d2de16b19dae068260c87c9373f5d
Lucky <luckykashyap3430@gmail.com>
2023-12-15 19:09:45 +0530

    authenticate user

--- backend/pinterest/routes/index.js
+++ backend/pinterest/routes/index.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
+const passport = require("passport");
+
+const localStrategy = require("passport-local");
+
+passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -10,4 +16,45 @@ router.get("/register", function (req, res, next) {
   res.render("register");
 });
 
+router.get("/profile", isloggedIn, function (req, res, next) {
+  res.render("profile");
+});
+
+router.post("/register", (req, res, next) => {
+  const data = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    contact: req.body.contact,
+  });
+
+  userModel.register(data, req.body.password).then(() => {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/",
+    successRedirect: "/profile",
+  }),
+  function (req, res, next) {}
+);
+
+router.get("/logout", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) next(err);
+
+    res.redirect("/");
+  });
+});
+
+function isloggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 module.exports = router;
--- backend/pinterest/routes/users.js
+++ backend/pinterest/routes/users.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/pinterest-clone");
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  boards: {
+    type: Array,
+    default: [],
+  },
+});
+
+userSchema.plugin(plm);
 
 module.exports = mongoose.model("user", userSchema);
