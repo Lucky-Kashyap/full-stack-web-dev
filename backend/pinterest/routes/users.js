cc242987645a7da74c8399dc54b5b71aa2fb27d7
Lucky <luckykashyap3430@gmail.com>
2023-12-15 18:48:59 +0530

    connect with database

--- backend/pinterest/routes/users.js
+++ backend/pinterest/routes/users.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest-clone");
 
-module.exports = router;
+const userSchema = mongoose.Schema({});
+
+module.exports = mongoose.model("user", userSchema);
