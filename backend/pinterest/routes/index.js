ec4f92025f0b4cb8648e7a026eb22d3660a09d0b
Lucky <luckykashyap3430@gmail.com>
2023-12-15 17:35:13 +0530

    register screen created

--- backend/pinterest/routes/index.js
+++ backend/pinterest/routes/index.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
   res.render("index");
 });
 
+router.get("/register", function (req, res, next) {
+  res.render("register");
+});
+
 module.exports = router;
