5317d59ff1febdba536ac341e650aa1289384535
Lucky <luckykashyap3430@gmail.com>
2024-01-04 15:15:19 +0530

    navbar created

--- front end/Tailwind Project/Paytm/tailwind.config.js
+++ front end/Tailwind Project/Paytm/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nilla: "#00baf2",
+        darkNilla: "#002970",
+        lightColor: "#f5f5f5",
+        lightNilla: "#00afe3",
+        lightDarkNilla: "#0f4a8a",
+        bgColor: "#f5f7fa",
+      },
+    },
   },
   plugins: [],
 };
